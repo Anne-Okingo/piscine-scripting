9f2242c4d7f7600f07d2b5752e514ec1de65eb8d
Anne-Okingo <annekadiso@gmail.com>
2025-12-03 14:33:49 +0300

    add script file that uses grep, a command-line utility for searching plain-text data sets for lines that match a regular expression

---
+++ file-researcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Find the sentence that starts with  "It takes 12 honey"
+grep "^It takes 12 honey" file-researcher/facts.txt
+
+
+#Find all lines containing the word "year"
+grep "year" file-researcher/facts.txt
