d53c836e7aafec8c412d67a2c92a23ef9cd70b9e
Anne-Okingo <annekadiso@gmail.com>
2025-12-04 07:31:39 +0300

    add script file with Creating variables

---
+++ set-internal-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MY_MESSAGE="Hello World"
+MY_NUM=100
+MY_PI=3.142
+MY_ARR=(one two three four five)
+
+echo "$MY_MESSAGE"
+echo "$MY_NUM"
+echo "$MY_PI"
+
+# Print array as: one, two, three, four, five
+echo "${MY_ARR[*]}" | sed 's/ /, /g'
