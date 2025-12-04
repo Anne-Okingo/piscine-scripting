4f722408054f691961229044a7d733c07d275bfc
Anne-Okingo <annekadiso@gmail.com>
2025-12-04 07:02:34 +0300

    add script file with file permission configuration

--- file-details.sh
+++ file-details.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-DIR="hard-perm"
-
-if [ ! -d "$DIR" ]; then
-    echo "Error: '$DIR' folder not found. Run this script from the directory containing '$DIR'."
-    exit 1
-fi
-
-ls -l --time-style="+%F %R" "$DIR" \
-    | sed '1d' \
-    | awk '{print $1, $6, $7, $8}'
+ls -l --time-style="+%F %R" hard-perm | sed '1d' | awk '{print $1, $6, $7, $5, $8}'
