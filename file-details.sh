aaed14c4b8b64f516abee7c2e919b8321270938a
Anne-Okingo <annekadiso@gmail.com>
2025-12-04 07:07:11 +0300

    add script file with file permission configuration

--- file-details.sh
+++ file-details.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -l --time-style="+%F %R" hard-perm | sed '1d' | awk '{print $1, $6, $7, $5, $8}'
+ls -l --time-style="+%F %R" hard-perm 2>/dev/null | sed '1d' | awk '{print $1, $6, $7, $5, $8}'
