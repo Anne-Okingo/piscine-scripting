a6c97083579588432a4942820b77126d7eb38eed
Anne-Okingo <annekadiso@gmail.com>
2025-12-04 06:42:19 +0300

    add script file with file permission configuration

---
+++ hard-perm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# change directory into the target folder
+cd hard-perm
+
+# Directories
+chmod 401 0
+chmod 777 3
+chmod 401 A
+
+# Files
+chmod 402 1
+chmod 604 2
+chmod 510 4
+chmod 460 5
+chmod 460 6
+chmod 510 7
+chmod 604 8
+chmod 402 9
