eb418a42f519565bdfdfa12aea5b0e2df1c056d7
Anne-Okingo <annekadiso@gmail.com>
2025-12-07 17:23:31 +0300

    feat: add array-selector.sh

--- array-selector.sh
+++ array-selector.sh
@@ -1,31 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define array
-colors=("red" "blue" "green" "white" "black")
+array=("red" "blue" "green" "white" "black")
 
-# Error if number of arguments is not 1
-if [[ $# -ne 1 ]]; then
+if [[ "$#" -ne 1 ]] || ! [[ $1 =~ ^-?[0-9]+$ ]]; then
     echo "Error"
+else
+    index=$(($1 - 1))
+
+    if [[ $index -lt 0 ]] || [[ $index -ge ${#array[@]} ]]; then
+        echo "Error"
+    else
+        echo "${array[$index]}"
+    fi
 fi
-
-arg="$1"
-
-re='^[+-]?[0-9]+$'
-
-
-# Check if argument is a number (integer only)
-if ! [[ "$arg" =~ $re ]]; then
-    echo "Error"
-
-fi
-
-# Convert 1-based input to 0-based index
-index=$((arg - 1))
-
-# Check range
-if [[ $index -lt 0 || $index -ge ${#colors[@]} ]]; then
-    echo "Error"
-fi
-
-# Print element
-echo "${colors[$index]}"
